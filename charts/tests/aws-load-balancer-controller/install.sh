55f0e76c77918a145b9cb66a1d7d8c9b20b1a818
guy-frontegg <guy@frontegg.com>
2025-06-17 11:50:04 +0300

    delete test folder

--- charts/tests/aws-load-balancer-controller/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env/bash
-
-set -e
-
-echo start
-helm repo add eks https://aws.github.io/eks-charts
-helm repo update
-helm upgrade --install aws-load-balancer-controller eks/aws-load-balancer-controller --version 1.12 --namespace load-balancer --create-namespace -f aws-load-balancer-controller.yaml
-echo done
--- charts/tests/nginx/install.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -e
-
-echo start
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx --version 4.8.2 -f nginx.values.yaml
-kubectl apply -f additional-objects/*
-echo done fuck you
